97a87fb715d334c62e48ca7aad61d925aab7f9cd
Pablo N Dalmasso <pdalmasso@MacBook-Pro-de-Pablo.local>
2015-09-12 19:34:58 -0300

    Se ahace el commit inicial

---
+++ Source/QuickStart/FloatingActor.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "QuickStart.h"
+#include "FloatingActor.h"
+
+
+// Sets default values
+AFloatingActor::AFloatingActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFloatingActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloatingActor::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+    FVector NewLocation = GetActorLocation();
+    float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+    NewLocation.Z += DeltaHeight * 30.0f;       //Scale our height by a factor of 20
+    NewLocation.X += DeltaHeight * 3000.0f;       //Scale our height by a factor of 20
+    RunningTime += DeltaTime;
+    SetActorLocation(NewLocation);
+}
+
+
+float RunningTime;
